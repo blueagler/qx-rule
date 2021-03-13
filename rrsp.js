e11e535f11dcc4149cae38730995f4a4e5d4f1c4
liujunhao <austinliu@blueagle.top>
2021-03-14 05:26:58 +0800

    first commit

---
+++ rrsp.js
@@ -0,0 +1,20 @@
+//^https:\/\/api.rr.tv(\/user\/profile*|\/drama\/app\/get_combined_drama_detail*)
+
+let { body, url } = $response;
+
+if (url.indexOf("/user/profile") != -1) {
+    var obj = JSON.parse(body);
+    obj["data"]["user"]["level"] = 30;
+    obj["data"]["user"]["vipMedal"] = 1;
+    obj["data"]["user"]["privilegeList"] = [1, 4, 37, 43, 23];
+} else if (url.indexOf("/drama/app/get_combined_drama_detail") != -1) {
+    var obj = JSON.parse(body);
+    obj["data"]["userVod"]["hasPrivilege"] = true;
+    obj["data"]["dramaDetail"]["season"]["downloadBlock"] = false;
+    obj["data"]["qualityConfig"]["sortedItems"] = [1, 4, 37, 43, 23];
+    for (let i = 0; i < obj["data"]["qualityConfig"]["sortedItems"].length; i++){
+        obj["data"]["qualityConfig"]["sortedItems"][i]["canPlay"] = true;
+    }
+}
+body = JSON.stringify(obj);
+$done({ body });
