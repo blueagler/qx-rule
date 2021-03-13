165222abbf340cef666461ce442b2330042800e1
liujunhao <austinliu@blueagle.top>
2021-03-14 06:50:50 +0800

    test

--- yddict.js
+++ yddict.js
@@ -1,5 +1,4 @@
 //^https:\/\/(ke|wow).youdao.com(\/user\/home*|\/course3\/api\/content\/ios\/applepay*)
-https://ke.youdao.com/course3/api/content/ios/applepay?Mkt=AppStore&Mkt1st=AppStore&Pdt=mdict.ios&client=iPhoneDict&deviceid=0eb17d8b3327484d4f4ffd7ca4be23fb&imei=0eb17d8b3327484d4f4ffd7ca4be23fb&keyfrom=mdict.9.0.16.iphonepro&mid=9.0.16&model=iPhone&product=pro&vendor=iPhone&visitorMode=false
 var body = $response.body;
 var url = $request.url;
 
