12812ff6ec9982c9f6f2d7a200c6a19db3ccc429
liujunhao <austinliu@blueagle.top>
2021-03-24 00:40:18 +0000

    test

--- rrsp.js
+++ rrsp.js
@@ -6,52 +6,6 @@ if (url.indexOf("/user/profile") != -1) {
     var obj = JSON.parse(body);
     obj["data"]["user"]["level"] = 30;
     obj["data"]["user"]["vipMedal"] = 1;
-    obj["data"]["user"]["privilegeList"] = [{
-		"action": "play",
-		"effectObject": "video",
-		"id": 1,
-		"function": "originalPainting",
-		"func": "originalPainting",
-		"endTime": 1667341767582,
-		"description": "解锁原画",
-		"icon": "jiesuoyuanhua"
-	}, {
-		"action": "play",
-		"effectObject": "video",
-		"id": 4,
-		"function": "noLimit",
-		"func": "noLimit",
-		"endTime": 1567341767582,
-		"description": "看剧无限制",
-		"icon": "kanjuwuxianzhi"
-	}, {
-		"action": "play",
-		"effectObject": "growth",
-		"id": 37,
-		"function": "0.4",
-		"func": "0.4",
-		"endTime": 1667341767582,
-		"description": "看剧经验+40%",
-		"icon": "jingyanzhijiacheng"
-	}, {
-		"action": "send",
-		"effectObject": "danmu",
-		"id": 43,
-		"function": "superBarrageBlue",
-		"func": "superBarrageBlue",
-		"endTime": 1667341767582,
-		"description": "超级弹幕",
-		"icon": "chaojidanmu"
-	}, {
-		"action": "play",
-		"effectObject": "video",
-		"id": 23,
-		"function": "noAd",
-		"func": "noAd",
-		"endTime": 1667341767582,
-		"description": "看剧无广告",
-		"icon": "kanjuwuguanggao"
-	}];
 } else if (url.indexOf("/drama/app/get_combined_drama_detail") != -1) {
     var obj = JSON.parse(body);
     obj["data"]["userVod"]["hasPrivilege"] = true;
