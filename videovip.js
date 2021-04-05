e6e5ea7c204bb5dd27a4f119c366a5dfbfde82f6
liujunhao <austinliu@blueagle.top>
2021-04-05 07:52:11 +0000

    test

--- videovip.js
+++ videovip.js
@@ -1,9 +1,9 @@
 //^https:\/\/(passport.iqiyi.com\/apis\/user\/info.action*|nuc.api.mgtv.com\/GetUserInfo*)
-let { body, url } = $response;
-let obj;
+var body = $response.body;
+var url = $request.url;
+//爱奇艺
 if (url.indexOf("passport.iqiyi.com") != -1) {
-  //爱奇艺
-  obj = JSON.parse(body);
+  var obj = JSON.parse(body);
   obj["data"]["vip_list"][0]["longestDeadline"]["t"] = 99999999999999;
   obj["data"]["vip_list"][0]["autoRenew"] = "1";
   obj["data"]["vip_list"][0]["yearExpire"] = 0;
@@ -11,8 +11,7 @@ if (url.indexOf("passport.iqiyi.com") != -1) {
   obj["data"]["vip_list"][0]["level"] = "999";
   obj["data"]["vip_list"][0]["status"] = "1";
 } else if (url.indexOf("nuc.api.mgtv.com") != -1) {
-  //芒果TV
-  obj = JSON.parse(body);
+  var obj = JSON.parse(body);
   obj["data"]["vipinfo"]["isvip"] = 1;
   obj["data"]["vipinfo"]["type"] = "2";
   obj["data"]["vipinfo"]["vip_end_time"] = "Forever";
