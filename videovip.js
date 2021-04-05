272ce4d4fff25a859319e91f9ebdb4e8c9b01aa5
liujunhao <austinliu@blueagle.top>
2021-04-05 07:50:16 +0000

    test

--- videovip.js
+++ videovip.js
@@ -1,9 +1,9 @@
 //^https:\/\/(passport.iqiyi.com\/apis\/user\/info.action*|nuc.api.mgtv.com\/GetUserInfo*)
-var body = $response.body;
-var url = $request.url;
-//爱奇艺
+let { body, url } = $response;
+let obj;
 if (url.indexOf("passport.iqiyi.com") != -1) {
-  var obj = JSON.parse(body);
+  //爱奇艺
+  obj = JSON.parse(body);
   obj["data"]["vip_list"][0]["longestDeadline"]["t"] = 99999999999999;
   obj["data"]["vip_list"][0]["autoRenew"] = "1";
   obj["data"]["vip_list"][0]["yearExpire"] = 0;
@@ -11,11 +11,13 @@ if (url.indexOf("passport.iqiyi.com") != -1) {
   obj["data"]["vip_list"][0]["level"] = "999";
   obj["data"]["vip_list"][0]["status"] = "1";
 } else if (url.indexOf("nuc.api.mgtv.com") != -1) {
-  var obj = JSON.parse(body);
+  //芒果TV
+  obj = JSON.parse(body);
   obj["data"]["vipinfo"]["isvip"] = 1;
   obj["data"]["vipinfo"]["type"] = "2";
   obj["data"]["vipinfo"]["vip_end_time"] = "Forever";
   obj["data"]["vipinfo"]["growth"]["score"] = 999999999999;
   obj["data"]["vipinfo"]["growth"]["level"] = 999999999999;
 }
+body = JSON.stringify(obj);
 $done({ body });
