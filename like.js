cf18ff38f9575422a441ded31260096bdc963216
liujunhao <austinliu@blueagle.top>
2021-04-25 21:06:39 +0800

    test

--- like.js
+++ like.js
@@ -1,4 +1,4 @@
-//^https:\/\/(www.xiaohongshu.com\/api\/sns\/v6/\message\/detect*|api.weibo.cn\/2\/direct_messages\/user_list*)
+//^https:\/\/(www.xiaohongshu.com\/api\/sns\/v6/\message\/detect*|api.weibo.cn\/2\/direct_messages\/user_list*|api.zhihu.com\/notifications\/v3\/count\/v2*)
 var body = $response.body;
 var url = $request.url;
 
@@ -17,6 +17,14 @@ if (url.indexOf("www.xiaohongshu.com/api/sns/v6/message/detect") != -1) {
     obj["unread_count"]["ext_new"]["addfriends"]["count"] = 9999;
     obj["unread_count"]["ext_new"]["follower"]["count"] = 9999;
     obj["unread_count"]["ext_new"]["likeuser"]["count"] = 9999;
+} else if (url.indexOf("api.zhihu.com/notifications/v3/count/v2") != -1) {
+    var obj = JSON.parse(body);
+    obj["message"]["count"] = 9999;
+    obj["message"]["show_count"] = true;
+    obj["feed"]["count"] = 9999;
+    obj["feed"]["show_count"] = true;
+    obj["total"]["count"] = 9999;
+    obj["total"]["show_count"] = true;
 }
 body = JSON.stringify(obj);
 $done({ body });
