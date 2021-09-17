14e9d2f6db15d1c55916965f5109ecfcd514670b
Blueagle <austinliu@blueagle.top>
2021-09-17 22:08:32 +0800

    a

--- shanbei.js
+++ shanbei.js
@@ -1,4 +1,4 @@
-//^https:\/\/apiv3.shanbay.com\/wordsapp\/(vip\/user_item\/status|walkman\/user_walkman)
+//^https:\/\/apiv3.shanbay.com\/(wordsapp\/(vip\/user_item\/status|walkman\/user_walkman)|abc\/applets\/user_applets)
 let [{ body }, { url }] = [$response, $request],
 obj = JSON.parse(body);
 if (url.indexOf("wordsapp/vip/user_item/status") != -1) {
@@ -6,6 +6,12 @@ if (url.indexOf("wordsapp/vip/user_item/status") != -1) {
 } else if (url.indexOf("wordsapp/walkman/user_walkman") != -1) {
     obj["status"] = 1;
     obj["due-date"] = "2099-01-01";
+}
+else if (url.indexOf("abc/applets/user_applets") != -1) {
+    for (i = 0; i < obj["objects"].length; i++){
+        obj["objects"][i]["due_date"] = "2099-01-01";
+        obj["objects"][i]["status"] = "VALID";
+    }
 }
 body = JSON.stringify(obj);
 $done({ body });
