a169aaae8ebcd43d15d79e077688ae09361d5d68
Blueagle <austinliu@blueagle.top>
2021-09-17 22:12:03 +0800

    t

--- shanbei.js
+++ shanbei.js
@@ -5,12 +5,12 @@ if (url.indexOf("wordsapp/vip/user_item/status") != -1) {
     obj["status"] = "IS_VIP";
 } else if (url.indexOf("wordsapp/walkman/user_walkman") != -1) {
     obj["status"] = 1;
-    obj["due-date"] = "2099-01-01";
+    obj["due_date"] = "Austin已破解";
 }
 else if (url.indexOf("abc/applets/user_applets") != -1) {
     for (i = 0; i < obj["objects"].length; i++){
-        obj["objects"][i]["due_date"] = "2099-01-01";
-        obj["objects"][i]["status"] = "VALID";
+        obj["objects"][i]["due_date"] = "Austin已破解";
+        obj["objects"][i]["status"] = 1;
     }
 }
 body = JSON.stringify(obj);
