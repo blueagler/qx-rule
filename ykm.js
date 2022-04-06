d813026d65d4d8f7665c2b8050a38847669489e9
Blueagle <austinliu@blueagle.top>
2022-04-06 21:49:10 +0800

    t

--- ykm.js
+++ ykm.js
@@ -30,12 +30,12 @@ if (url.includes("ebus/minshengwxmp/api/r/opc_process/collection")) {
                     "接种时间": `2022-01-0${String(i)}`,
                     "是否完成接种": "是",
                     "申报时间": `2022-01-0${String(i)} 00:00:00`,
-                    "厂家名称": json.data.records[0] ? json.data.records[0]["厂家名称"] : config.vaccine_factory,
-                    "数据来源": "SZ",
-                    "疫苗名称": json.data.records[0] ? json.data.records[0]["疫苗名称"] : config.vaccine_name,
+                    "厂家名称": json.data.records?.[0]["厂家名称"] ?? config.vaccine_factory,
+                    "数据来源": json.data.records?.[0]["数据来源"] ?? "SZ",
+                    "疫苗名称": json.data.records?.[0]["疫苗名称"] ?? config.vaccine_name,
                     "针次": String(i),
-                    "接种单位": json.data.records[0] ? json.data.records[0]["接种单位"] : config.inoculation_unit,
-                    "批号": "202111093L"
+                    "接种单位": json.data.records?.[0]["接种单位"] ?? config.inoculation_unit,
+                    "批号": json.data.records?.[0]["批号"] ?? "202111093L"
                 })
             }
             json.data.records = newRecords;
@@ -43,16 +43,16 @@ if (url.includes("ebus/minshengwxmp/api/r/opc_process/collection")) {
         case url.includes("ykmindex/nat"):
             json.data.records = [
                 {
-                    "姓名": json.data.records[0] ? json.data.records[0]["姓名"] : config.name,
+                    "姓名": json.data.records?.[0]["姓名"] ?? config.name,
                     "检测结果": "阴性",
                     "申报时间": getTestTime(1, true),
-                    "数据来源": "公安",
+                    "数据来源": json.data.records?.[0]["数据来源"] ?? "公安",
                     "检测类型": "核酸检测",
                     "采样日期": getTestTime(1, true),
-                    "展示来源": "广东省卫生健康委员会",
+                    "展示来源": json.data.records?.[0]["展示来源"] ?? "广东省卫生健康委员会",
                     "检测日期": getTestTime(1, true),
                     "显示时间": getTestTime(1, false),
-                    "检测机构": json.data.records[0] ? json.data.records[0]["检测机构"] : config.testing_facility,
+                    "检测机构": json.data.records?.[0]["检测机构"] ?? config.testing_facility,
                 }
             ]
             break;
@@ -61,16 +61,16 @@ if (url.includes("ebus/minshengwxmp/api/r/opc_process/collection")) {
             let injectRecords = [];
             for (let i = 1; i <= 30; i++) {
                 injectRecords.push({
-                    "姓名": json.data.records[0] ? json.data.records[0]["姓名"] : config.name,
+                    "姓名": json.data.records?.[0]["姓名"] ?? config.name,
                     "检测结果": "阴性",
                     "申报时间": getTestTime(i, true),
-                    "数据来源": "国办",
+                    "数据来源": json.data.records?.[0]["数据来源"] ?? "国办",
                     "检测类型": "核酸检测",
                     "采样日期": getTestTime(i, true),
-                    "展示来源": "国家卫生健康委员会",
+                    "展示来源": json.data.records?.[0]["展示来源"] ?? "国家卫生健康委员会",
                     "检测日期": getTestTime(i, true),
                     "显示时间": getTestTime(i, false),
-                    "检测机构": json.data.records[0] ? json.data.records[0]["检测机构"] : config.testing_facility,
+                    "检测机构": json.data.records?.[0]["检测机构"] ?? config.testing_facility,
                 })
             }
 
