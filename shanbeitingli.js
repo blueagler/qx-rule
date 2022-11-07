8e264bdd258ad77901455f26be9b1909160ddf22
Blueagle <austinliu@blueagle.top>
2022-11-07 23:13:18 +0800

    t

---
+++ shanbeitingli.js
@@ -0,0 +1,14 @@
+//^https:\/\/apiv3.shanbay.com\/listen\/user\/vip
+let [{ body }, { url }] = [$response, $request],
+  obj = JSON.parse(body);
+obj = {
+  ...obj,
+  "vip_expire_at": "2099-01-01",
+  "is_vip": true,
+  "vip_remain_days": 99999999,
+  "vip_valid_at": "2099-01-01",
+  "is_big_member": true,
+  "big_member_expire_at": "2099-01-01"
+}
+body = JSON.stringify(obj);
+$done({ body });
