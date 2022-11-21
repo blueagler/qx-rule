86d7a38f089e7e70dd60c18f29281f3e89d48e3b
Blueagle <austinliu@blueagle.top>
2022-11-21 23:02:24 +0800

    t

--- qita.js
+++
@@ -1,9 +0,0 @@
-//^https:\/\/speak.api.yingpar.com\/user\/(.*)/info
-let obj = JSON.parse($response.body);
-obj["data"] = {
-  ...obj["data"],
-  "vipEndTime": 1692502399,
-  "vipType": 1,
-  "vipLevel": 1
-}
-$done({ body: JSON.stringify(obj) });
