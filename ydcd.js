f5c9d27439b297de07a5d39ffa32fd0f7eb25d83
Blueagle <austinliu@blueagle.top>
2022-11-24 00:47:36 +0800

    t

--- ydcd.js
+++ ydcd.js
@@ -1,13 +1,11 @@
-//^https:\/\/dict.youdao.com\/vip\/user\/status
+//^https:\/\/dict.youdao.com\/vip\/user\/status?
 let obj = JSON.parse($response.body);
 obj = {
   ...obj,
-  "svipExpire": 1722386993000,
-  "svip": true,
-  "expire": 1722386993000,
+  "expire": 1822386993000,
   "vip": true,
   "purchasedPlanIds": [
-    "29"
+    "32"
   ],
   "copyWritings": [
     "Blueagle已破解会员"
