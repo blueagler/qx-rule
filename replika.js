2e236c5bedf5413bfef49148cdbb17a4546d71c1
Blueagle <austinliu@blueagle.top>
2023-01-07 18:03:50 +0800

    t

--- replika.js
+++ replika.js
@@ -1,16 +1,32 @@
-//^https:\/\/replika.ai\/api\/mobile\/1.5\/payment_subscriptions
+//^https:\/\/replika.ai\/api\/mobile\/1.5\/(payment_subscriptions|store\/login_reward|personal_bot)
 let [{ body }, { url }] = [$response, $request],
   obj = JSON.parse(body),
   path = new URL(url).pathname;
 switch (path) {
   case "/api/mobile/1.5/payment_subscriptions":
     obj["features"] = [
-      { "name": "ar_calls" },
-      { "name": "voice_message" },
-      { "name": "backgrounds" },
-      { "name": "send_photo" },
+      ...new Set([
+        ...obj["unlocked_features"],
+        { "name": "ar_calls" },
+        { "name": "voice_message" },
+        { "name": "backgrounds" },
+        { "name": "send_photo" },
+      ])
     ];
     break;
+  case "/api/mobile/1.5/store/login_reward":
+    obj["has_subscription"] = true;
+    break;
+  case "/api/mobile/1.5/personal_bot":
+    obj["unlocked_features"] = [
+      ...new Set([
+        ...obj["unlocked_features"],
+        "ar_calls",
+        "voice_message",
+        "backgrounds",
+        "send_photo"
+      ])
+    ]
 }
 body = JSON.stringify(obj);
 $done({ body });
