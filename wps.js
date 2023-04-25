914347a90ddbd1e38b6dcf45d752edbac9524440
Blueagle <austinliu@blueagle.top>
2023-04-25 18:42:04 +0800

    t

--- wps.js
+++ wps.js
@@ -1,4 +1,12 @@
 //^https:\/\/account\.wps\.cn\/api\/users
+
+const $response = {
+  body: `{"exp":0,"level":1,"privilege":[],"result":"ok","server_time":1682419211,"total_buy":0,"total_cost":0,"userid":470102475,"vip":{"name":"注册用户","has_ad":1,"memberid":10,"expire_time":1993459211,"enabled":[]},"wealth":0}`
+}
+const $request = {
+  url: "https://account.wps.cn/api/users",
+}
+
 let [{ body }, { url }] = [$response, $request];
 
 const handler = {
@@ -28,10 +36,11 @@ const handler = {
     }
   })
 };
-for (const p in handler)
-  if (new RegExp(p).test(path)) {
+for (const p in handler) {
+  if (new RegExp(p).test(url)) {
     body = JSON.stringify(handler[p](JSON.parse(body)));
     break;
   }
+}
 
 $done({ body });
