db417b0449dfc5038fd47cef81a08fe59dd84456
Blueagle <austinliu@blueagle.top>
2024-02-15 03:38:30 +0800

    Update status value for VIPs

--- jkydt.js
+++ jkydt.js
@@ -10,7 +10,7 @@ const handlers = [
       for (const vipKey in obj.result) {
         obj.result[vipKey].beginTime = "1970-01-01";
         obj.result[vipKey].endTime = "2099-12-31";
-        obj.result[vipKey].status = "0";
+        obj.result[vipKey].status = "1";
         obj.result[vipKey].level = 1;
       }
       return obj;
