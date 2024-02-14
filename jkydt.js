773fa4c55de97464d290863e94af54765b3db032
Blueagle <austinliu@blueagle.top>
2024-02-15 03:42:13 +0800

    Refactor VIP rights handling

--- jkydt.js
+++ jkydt.js
@@ -1,6 +1,14 @@
-//^https:\/\/vipapi.ksedt.com\/store\/batchCheckRights
+//^https:\/\/vipapi.ksedt.com\/store\/(batchCheckRights|checkRights)
 
 let [{ body }, { url }] = [$response, $request];
+
+function modifyRights(obj) {
+  obj.beginTime = "1970-01-01";
+  obj.endTime = "2099-12-31";
+  obj.status = "2";
+  obj.level = 1;
+  return obj;
+}
 
 const handlers = [
   {
@@ -8,11 +16,16 @@ const handlers = [
     type: "JSON",
     handler: (obj) => {
       for (const vipKey in obj.result) {
-        obj.result[vipKey].beginTime = "1970-01-01";
-        obj.result[vipKey].endTime = "2099-12-31";
-        obj.result[vipKey].status = "1";
-        obj.result[vipKey].level = 1;
+        obj.result[vipKey] = modifyRights(obj.result[vipKey]);
       }
+      return obj;
+    },
+  },
+  {
+    regex: /checkRights/,
+    type: "JSON",
+    handler: (obj) => {
+      obj.result = modifyRights(obj.result);
       return obj;
     },
   },
